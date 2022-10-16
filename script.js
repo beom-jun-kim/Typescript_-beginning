c9c6523e9c66f3417fd1ebb1378bd3819361e7ad
kimbeomjun <gimb52043@gmail.com>
2022-10-16 21:36:39 +0900

    Typescript_begining

---
+++ script.js
@@ -0,0 +1,26 @@
+
+// 1.
+// const nico = {
+//   nickname:"nick"
+// }
+
+// nico.hello()
+// return X
+
+// 2.
+// [1,2,3,4] + false
+// return X
+
+// 3.
+// function divide(a, b) {
+//     return a / b
+// }
+// divide("hello")
+// return X
+
+// 4.
+// const player = {
+//     age:12
+// }
+// player.age = false
+// return X
